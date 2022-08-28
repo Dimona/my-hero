582d5cb5cf8b35b67c36ee51b7f046825acc5003
Dmytro Ieremenko <dieremenko@mindk.com>
2022-08-28 20:30:40 +0300

    feat: refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { GameCommand } from '@app/../game/commands/game.command';
 import { WinstonModule } from 'nest-winston';
 import { winstonLoggerConfig } from '@loggers/winston/winston.logger.config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { GameModule } from '@game/game.module';
 import { OutputModule } from '@output/output.module';
 import { ContextModule } from '@context/context.module';
-import { GameQuestions } from '@app/../game/interactive/game.question';
 
 @Module({
   imports: [
